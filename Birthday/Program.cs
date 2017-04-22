f7629045b3b52668240c5cef33a99027a0ec53ce
Nikita <nikitagorinvg@gmail.com>
2017-04-22 13:34:52 +0300

    Осталось дней до др

---
+++ Birthday/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.SetWindowSize(55, 10);
+            DateTime now = DateTime.Now;
+            DateTime birthDay;
+            TimeSpan daysToBirth;
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Введите Вашу дату рождения в формате 'гггг.мм.дд'");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+        
+                birthDay = DateTime.Parse(Console.ReadLine());
+
+                DateTime thisyear = new DateTime(now.Year, birthDay.Month, birthDay.Day);//этот год и мой месяц дата(для расчета разницы)
+
+                if (thisyear < now)//если др уже был
+                {
+                    thisyear = new DateTime(now.Year + 1, birthDay.Month, birthDay.Day);
+                    daysToBirth = thisyear - now;
+                }
+                else
+                {
+                    daysToBirth = thisyear - now;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("До дня рождения осталось {0} дня(ей)", daysToBirth.Days);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Хотите продолжить? y/n");
+                if (Console.ReadLine() == "n")
+                {
+                    flag = false;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
